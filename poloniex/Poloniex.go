2a7f65d013836e2f2d53cef5a7d989a1dfef411c
qct <quchentao@163.com>
2017-08-24 23:49:08 +0800

    get all currencies

--- poloniex/Poloniex.go
+++ poloniex/Poloniex.go
@@ -639,3 +639,29 @@ func (poloniex *Poloniex) GetCurrency(currency Currency) (*PoloniexCurrency, err
 
 	return poloniexCurrency, nil
 }
+
+func (poloniex *Poloniex) GetAllCurrencies() (map[string]*PoloniexCurrency, error) {
+    respmap, err := HttpGet(poloniex.client, PUBLIC_URL + CURRENCIES_API)
+
+    if err != nil || respmap["error"] != nil {
+        log.Println(err)
+        return nil, err
+    }
+
+    result := map[string]*PoloniexCurrency{}
+    for k, v := range respmap {
+        currencyMap := v.(map[string]interface{})
+        poloniexCurrency := new(PoloniexCurrency)
+        poloniexCurrency.ID = int(currencyMap["id"].(float64))
+        poloniexCurrency.Name, _ = currencyMap["name"].(string)
+        poloniexCurrency.TxFee, _ = strconv.ParseFloat(currencyMap["txFee"].(string), 64)
+        poloniexCurrency.MinConf = int(currencyMap["minConf"].(float64))
+        poloniexCurrency.DepositAddress, _ = currencyMap["depositAddress"].(string)
+        poloniexCurrency.Disabled = int(currencyMap["disabled"].(float64))
+        poloniexCurrency.Delisted = int(currencyMap["delisted"].(float64))
+        poloniexCurrency.Frozen = int(currencyMap["frozen"].(float64))
+
+        result[k] = poloniexCurrency
+    }
+    return result, nil
+}
