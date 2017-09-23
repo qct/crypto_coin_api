1f18cbd602d44627c7e33b1e55505a81d1a763be
qct <quchentao@163.com>
2017-09-23 10:01:50 +0000

    add StringToCurrencyPair

--- currency_pair.go
+++ currency_pair.go
@@ -14,6 +14,17 @@ func NewCurrencyPair(base Currency, counter Currency) CurrencyPair {
 	return CurrencyPair{base, counter}
 }
 
+func StringToCurrencyPair(cp, sp string) CurrencyPair {
+	if sp == "" {
+        return CurrencyPair{}
+    }
+    split := strings.Split(cp, sp)
+    if len(split) != 2 {
+        return CurrencyPair{}
+    }
+    return CurrencyPair{BaseCurrency: Currency(split[0]), CounterCurrency: Currency(split[1])}
+}
+
 func (cp CurrencyPair) Symbol() string {
 	return cp.CustomSymbol("_", false)
 }
